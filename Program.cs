9ea66c640856a5ccad64be751ec91cbf0652783d
Luélly Cabral <luellycabral21@gmail.com>
2020-10-06 16:02:25 -0300

    Adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double Base;
+           double Altura;
+           double Area;
+
+            Console.Write("1 Coloque o valor da base: ");
+          Base = double.Parse( Console.ReadLine());
+
+            Console.Write("2 Coloque o valor de altura: ");
+            Altura = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine($"A área do triangulo é: {Area = Base * Altura / 2} (cm²)");
+
+
+
+            
+            
+
+
+        }
+    }
+}
